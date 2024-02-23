33d108782520b051583c267dfae2c18b7ffe428d
purplessmoke <PolinaSt.2003@yandex.ru>
2024-02-23 18:52:20 +0300

    создание слайдера

--- src/components/ProductsContainer/ProductCard/ProductCard.jsx
+++ src/components/ProductsContainer/ProductCard/ProductCard.jsx
@@ -1,15 +1,16 @@
 import {FaRegStar} from "react-icons/fa";
+import { IoBagOutline } from "react-icons/io5";
 import styles from "./ProductCard.module.css"
-import {Button} from "../../Button/Button";
 export const ProductCard = ({image, name, price}) => {
     return (
         <div className={styles.ProductCard}>
-            <FaRegStar className={styles.Star}/>
+            <div className={styles.IconGroup}>
+                <FaRegStar/>
+                <IoBagOutline/>
+            </div>
             <img src={image} alt={name} className={styles.ProductCardImg}/>
             <h3 className={styles.ProductCardName}>{name}</h3>
             <p className={styles.ProductCardPrice}>{price} ₽</p>
-             <Button>Добавить в корзину</Button>
-
         </div>
     )
 }
--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -1,9 +1,9 @@
 import styles from "./ProductsContainer.module.css"
 
 import {ProductCard} from "./ProductCard/ProductCard";
+import {Slider} from "../Slider/Slider";
 
 export const ProductsContainer = ({title, products}) => {
-    console.log(products)
     return (
         <section>
             <div className={styles.ProductsContainer}>
@@ -11,11 +11,12 @@ export const ProductsContainer = ({title, products}) => {
                 <p className={styles.MoreProducts}>Больше товаров</p>
             </div>
             <div className={styles.ProductCardList}>
-                {products?.map(product => <ProductCard key={product.id}
-                                                       image={`http://192.168.0.104:5555/${product.image}`}
-                                                       price={product.price}
-                                                       name={product?.productName}
-                />)}
+                {/*{products?.map(product => <ProductCard key={product.id}*/}
+                {/*                                       image={`http://192.168.0.104:5555/${product.image}`}*/}
+                {/*                                       price={product.price}*/}
+                {/*                                       name={product?.productName}*/}
+                {/*/>)}*/}
+                {products && <Slider slidesNumber={4} slides={products}/>}
             </div>
         </section>
     )
