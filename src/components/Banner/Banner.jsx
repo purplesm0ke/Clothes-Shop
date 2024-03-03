e073ac39c42d50955995e391967e91e2a4124d43
purplessmoke <PolinaSt.2003@yandex.ru>
2024-03-03 19:54:59 +0300

    дополнение карточки товара, изменение кнопки

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,13 @@
 import banner from '../../assets/images/banner.jpg'
 import styles from './Banner.module.css'
+import {Button} from "../Button/Button";
 export const Banner = () => {
     return (
         <div className={styles.Banner_location}>
             <div className={styles.Banner_text_location}>
                 <h1 className={styles.Banner_title}>Широкий <br/> ассортимент <br/> Одежды</h1>
                 <p className={styles.Banner_text}>Одежда от известные брендов у нас в каталоге. <br/> Только качественные вещи.</p>
-                <p className={styles.Banner_btn}>Перейти в каталог</p>
+                <Button>Перейти в каталог</Button>
             </div>
         </div>
 
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,28 @@
 import {useParams} from "react-router-dom";
+import {Header} from "../../layout/MainLayout/Header/Header";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import styles from "./ProductPage.module.css"
+import {Button} from "../../components/Button/Button";
 
-export const ProductPage = ()  => {
-    const params = useParams()
-    console.log(params)
-    return(
-        <div>123</div>
+export const ProductPage = () => {
+    const {productId} = useParams()
+    const [product, setProduct] = useState()
+    useEffect(() => {
+        axios.get(`http://192.168.0.104:5555/product/${productId}`).then((res) => setProduct(res.data))
+    }, [])
+    return (
+        <div>
+            <Header/>
+            <div className={styles.ProductPlace}>
+                <img className={styles.ProductImg} src={`http://192.168.0.104:5555/${product?.image}`} alt=""/>
+                <div className={styles.ProductInfo}>
+                    <p>{product?.productName}</p>
+                    <p>{product?.price} ₽</p>
+                    <p>Размер</p>
+                    <Button>Добавить  в корзину</Button>
+                </div>
+            </div>
+        </div>
     )
 }
